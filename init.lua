f3e7dce356ea47608d637e0806755720a8b248af
idan <idandrori555@gmail.com>
2025-11-07 17:25:13 +0200

    added ALT tab navigation

--- init.lua
+++ init.lua
@@ -204,6 +204,11 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Map ALT + N to each tab
+for i = 1, 9 do
+  vim.keymap.set('n', '<M-' .. i .. '>', i .. 'gt', { noremap = true, silent = true })
+end
+
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
