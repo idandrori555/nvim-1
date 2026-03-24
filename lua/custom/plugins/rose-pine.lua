c9f2fe4f9b3335be70ed9239063d1113ed09b60f
idan <idandrori555@gmail.com>
2026-03-24 18:14:23 +0200

    updated rose-pine to be trans

--- lua/custom/plugins/rose-pine.lua
+++ lua/custom/plugins/rose-pine.lua
@@ -2,6 +2,13 @@ return {
   'rose-pine/neovim',
   name = 'rose-pine',
   config = function()
+    require('rose-pine').setup {
+      variant = 'auto',
+      styles = {
+        transparency = true,
+      },
+    }
+
     vim.cmd 'colorscheme rose-pine'
   end,
 }
